81a34893b7a40395a3cd1eff9e61286c610dcc38
willmvc2 <willmvc@gmail.com>
2026-02-04 16:34:55 -0300

    Increase font sizes in app styling

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.set_page_config(
 st.markdown("""
 <style>
 .stApp { background-color: #2b59b4; color: white; }
-p, span, div { font-size: 18px; }
+p, span, div { font-size: 30px; }
 h1 { font-size: 60px; color: white; }
 h3 { font-size: 24px; color: white; }
 input { font-size: 20px !important; }
